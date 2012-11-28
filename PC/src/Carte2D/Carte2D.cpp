de409b223f707314b4fc2dca660c8e38408268f8
Atelier Piston ROBotique <club-robot@sympa.ec-lyon.fr>
2012-11-28 23:21:46 +0100

    Modification du code de la Carte2D our fonctionner sous Windows

--- PC/src/Carte2D/Carte2D.cpp
+++ PC/src/Carte2D/Carte2D.cpp
@@ -16,19 +16,6 @@ Carte2D::Carte2D(std::string nomFichierFond, double x0rigine, double yOrigine, d
     //Choix du centre et mise à l'échelle du sprite pour correspondre au modèle
     spriteFond.SetCenter(x0rigine, yOrigine);
     spriteFond.SetScale(echelleX, echelleY);
-
-    //Création de la fenêtre permettant d'afficher la carte : choix -> même dimensions que l'image
-    fenetre.Create(sf::VideoMode(imageFond.GetWidth(), imageFond.GetHeight()), "carte 2D");
-
-    //On nettoie la fenêtre en la remplissant de noir (inutile mais c'est pour montrer la syntaxe)
-    //fenetre.Clear();
-
-    sf::Vector2f origineGlobale=spriteFond.TransformToGlobal(sf::Vector2f(0,0));
-    //Création d'une vue permettant que le fond occupe tout l'écran malgré le fait qu'on aie chagé de coordonnées
-    vue.SetFromRect(sf::FloatRect(origineGlobale.x, origineGlobale.y, spriteFond.GetSize().x+origineGlobale.x, spriteFond.GetSize().y+origineGlobale.y));
-    fenetre.SetView(vue);
-
-    MettreAJour();
 }
 
 Carte2D::~Carte2D()
@@ -84,9 +71,27 @@ bool Carte2D::SetPeriodeRafraichissement(float p)
 
 void Carte2D::Run()
 {
+    //Création de la fenêtre permettant d'afficher la carte : choix -> même dimensions que l'image
+    fenetre.Create(sf::VideoMode(imageFond.GetWidth(), imageFond.GetHeight()), "carte 2D");
+
+    //On nettoie la fenêtre en la remplissant de noir (inutile mais c'est pour montrer la syntaxe)
+    fenetre.Clear();
+
+    sf::Vector2f origineGlobale=spriteFond.TransformToGlobal(sf::Vector2f(0,0));
+    //Création d'une vue permettant que le fond occupe tout l'écran malgré le fait qu'on aie chagé de coordonnées
+    vue.SetFromRect(sf::FloatRect(origineGlobale.x, origineGlobale.y, spriteFond.GetSize().x+origineGlobale.x, spriteFond.GetSize().y+origineGlobale.y));
+    fenetre.SetView(vue);
+
+
+    //L'interception des evenements est necessaire sous windows pour
+    //que la fenetre ne freeze pas
+    sf::Event eventUseless;
+
     rafraichissementAutomatique = true;
     while(rafraichissementAutomatique)
     {
+        fenetre.SetActive(true);
+        while(fenetre.GetEvent(eventUseless)){};
         MettreAJour();
         sf::Sleep(periodeRafraichissement);
     }
